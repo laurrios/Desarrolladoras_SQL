c036435441fd35ad928fe5c8e335efe6d888d214
laurrios <122172443+laurrios@users.noreply.github.com>
2023-05-09 19:23:44 +0200

    Add files via upload

---
+++ ActividadSQL_LauradelosRios.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `cursosql`.`usuario` 
+	(`ID` INT NOT NULL , 
+	`NOMBRE` VARCHAR(20) NOT NULL , 
+	`APELLIDO` VARCHAR(20) NOT NULL , 
+	`EMAIL` VARCHAR(20) NOT NULL , 
+	PRIMARY KEY (`ID`)) ENGINE = InnoDB;
+
+
+Actividad de Laura de los Rios Sandoval
